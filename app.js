b27ec836b3eed66f978671a8695cce4886e82ed5
Julio Mayén <mayenrosil@gmail.com>
2024-02-19 22:24:12 -0600

    change backend

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
 
-const version = '2';
+const version = '800';
 
 app.get('/', (req, res) => {
     // set response content    
         res.send(`<html>
                     <body>
-                        <h1 style="color:red;text-align: center;margin-top: 100px;"> [Version ${version}]: THis is AMAZING!!! Like & Subscribe!</h1>
+                        <h1 style="color:pink;text-align: center;margin-top: 100px;"> [Version ${version}]: Hola Kenny uwu</h1>
                         <div style="position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%)">
                             <img src="https://picsum.photos/400/400?random=1">
                         </div>
