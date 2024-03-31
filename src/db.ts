6f853abb0f2dda6a61e85721c96b609e12debaaf
Julio Mayén <mayenrosil@gmail.com>
2024-03-30 21:48:47 -0600

    update ip address

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import { User } from './entities/User';
 
 export const AppDataSource = new DataSource({
     type: 'mysql',
-    host: "localhost",
+    host: "50.17.111.232",
     port: 3306,
     username: "admin",
     password: "W3p@rd0$",
