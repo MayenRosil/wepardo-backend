37ffb8afb900702e12a4beb81f70a505d35617d7
Julio Mayén <mayenrosil@gmail.com>
2024-05-25 10:55:39 -0600

    upload to both directories in bucket

--- src/libs/manageS3.ts
+++ src/libs/manageS3.ts
@@ -29,20 +29,32 @@ export const uploadToS3 = async (rutaArchivoLocal: string, nombreArchivo: string
         const fileContent = fs.readFileSync(filePath);
         console.log(fileContent, 'el contenido')
 
-        const params = {
+        const params1 = {
           Bucket: 'wepardo-resources',
           Key: 'users/almacen/MayenRosil', // Nombre del archivo en S3
           Body: fileContent,
           ContentType: 'image/png', // Cambia esto si el tipo de archivo es diferente
         };
 
+        const params2 = {
+            Bucket: 'wepardo-resources',
+            Key: 'users/comparacion/MayenRosil', // Nombre del archivo en S3
+            Body: fileContent,
+            ContentType: 'image/png', // Cambia esto si el tipo de archivo es diferente
+          };
 
-        const command = new PutObjectCommand(params);
-        console.log(command, 'el comando');
-        console.log('-----');
-    
-        const response: PutObjectCommandOutput = await s3Client.send(command);
-        console.log(response, 'la respuesta');
+
+        const command1 = new PutObjectCommand(params1);
+        console.log(command1, 'el comando');
+        const response1: PutObjectCommandOutput = await s3Client.send(command1);
+        console.log(response1, 'la respuesta');
+
+        
+
+        const command2 = new PutObjectCommand(params2);
+        console.log(command2, 'el comando');
+        const response2: PutObjectCommandOutput = await s3Client.send(command2);
+        console.log(response2, 'la respuesta');
     
         console.log('Archivo subido exitosamente a S3');
         return true;
