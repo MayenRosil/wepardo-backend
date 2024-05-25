cd0454bd86f7aaf1cdc0c953736eb56a5edbeb87
Julio Mayén <mayenrosil@gmail.com>
2024-05-25 11:20:09 -0600

    update aws credentials

--- src/libs/manageS3.ts
+++ src/libs/manageS3.ts
@@ -74,12 +74,13 @@ export const compareFaces = async (): Promise<void> => {
     try {
 
         const rekognitionClient = new RekognitionClient({
-            region: process.env.AWS_REGION,
+     
             credentials: {
-              accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-              secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
-            }
-          });
+                accessKeyId: process.env.AWS_ACCESS_KEY || '',
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+            },
+            region: process.env.AWS_REGION || ''
+        });
 
       const params = {
         SourceImage: {
